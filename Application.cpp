eff6c17675b95d838267acd03eea282de6a11392
Alexandre Moraes <alcmoraes89@gmail.com>
2022-02-13 13:59:04 -0300

    First commit

---
+++ Application.cpp
@@ -0,0 +1,15 @@
+#include "Application.h"
+
+#include "imgui.h"
+
+namespace MyApp
+{
+    void RenderUI()
+    {
+        ImGui::Begin("Settings");
+        ImGui::Button("Hello");
+        static float value = 0.0f;
+        ImGui::DragFloat("Value", &value);
+        ImGui::End();
+    }
+}
